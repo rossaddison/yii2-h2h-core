b576703f34493f2da63ce94015b36168d1c02866
Ross Addison <rossaddison@users.noreply.github.com>
2021-05-03 17:14:05 +0100

    Update _expandableviewdebtsheet.php

--- frontend/views/product/_expandableviewdebtsheet.php
+++ frontend/views/product/_expandableviewdebtsheet.php
@@ -8,14 +8,17 @@ use frontend\models\Salesorderheader;
 use Yii;
 $tooltipview = Html::tag('span', Yii::t('app','View'), ['title'=>Yii::t('app','View or Update or Delete'),'data-toggle'=>'tooltip','style'=>'text-decoration: underline: cursor:pointer;']);
 $tooltippayoff = Html::tag('span', Yii::t('app','Unpaid'), ['title'=>Yii::t('app','Record as paid.'),'data-toggle'=>'tooltip','style'=>'text-decoration: underline: cursor:pointer;']);
+$tooltippdf = Html::tag('span', Yii::t('app','Pdf'), ['title'=>Yii::t('app','Pdf'),'data-toggle'=>'tooltip','style'=>'text-decoration: underline: cursor:pointer;']);
 $tooltipdailycleandate  = Html::tag('span', Yii::t('app','Date'), ['title'=>Yii::t('app','Clean Date of Daily Clean'),'data-toggle'=>'tooltip','style'=>'text-decoration: underline: cursor:pointer;']);
 $tooltipsalesorderheader = Html::tag('span', 'SOH', ['title'=>Yii::t('app','Sales Order Header'),'data-toggle'=>'tooltip','style'=>'text-decoration: underline: cursor:pointer;']);
 $tooltipsalesorderdetail = Html::tag('span', 'SOD', ['title'=>Yii::t('app','Sales Order Detail'),'data-toggle'=>'tooltip','style'=>'text-decoration: underline: cursor:pointer;']);
 ?>
 <div class="product-expandable-viewdebtsheet">
-<button id="w178" class = "btn btn-success" onclick="js:getPaidallowing()"><?php echo Yii::t('app','Pay off all (ticked)') ?></button> 
+
+
     <?php
         $db=\frontend\components\Utilities::userdb();
+        
         $q = new Query;
         $rows = $q->select('*')
         ->from('works_salesorderdetail')
@@ -24,7 +27,25 @@ $tooltipsalesorderdetail = Html::tag('span', 'SOD', ['title'=>Yii::t('app','Sale
         ->andWhere('paid=0.00')    
         //->andWhere('sales_order_detail_id<'.$model->sales_order_detail_id)
         ->all($db);
-?>  
+        
+        //at least one salesorderdetail has no invoice associated with it therefore enable the generate button        
+        $w = new Query;
+        $count = $w->select('*')
+        ->from('works_salesorderdetail')
+        ->andWhere('product_id='.$model->id)
+        ->andWhere('unit_price>0.00')
+        ->andWhere('paid=0.00')
+        ->andWhere(['invoice_id'=> null])        
+        //->andWhere('sales_order_detail_id<'.$model->sales_order_detail_id)
+        ->all($db);
+        
+    ?> 
+
+    <?php if (!empty($count)){
+        echo Yii::t('app','<button id="w178" class = "btn btn-success" onclick="js:getPaidallowing()">Pay off all (ticked)</button><br><br>');  
+        echo Yii::t('app','<button id="w188" class = "btn btn-success" onclick="js:getGenerateinvoice()">Generate Invoice (ticked)</button>'); 
+    } ?>
+     
 <?php
 $data = $rows;
 $provider = new ArrayDataProvider([
@@ -58,6 +79,14 @@ $provider = new ArrayDataProvider([
     'columns' => [
         [
          'class'=>'kartik\grid\CheckBoxColumn',
+         'visible'=> !empty($count),
+         'checkboxOptions' => function($provider) {
+                if(!empty($provider['invoice_id'])){
+                   return ['disabled' => true];
+                }else{
+                   return [];
+                }
+         },
          'multiple'=>false, 
         ],    
         ['class' => 'kartik\grid\SerialColumn'],
@@ -118,10 +147,11 @@ $provider = new ArrayDataProvider([
                     'buttons' => [
                             'link' => function ($url, $provider,$key) {
                             if  ($provider['paid'] < $provider['unit_price']){
-                            return Html::a(Yii::t('app','Unpaid'), $url,['class' => 'btn btn-danger']);
-                            }
-                            if  ($provider['paid'] = $provider['unit_price']){
-                            return Html::a(Yii::t('app','Paid'), $url,['class' => 'btn btn-danger']);
+                               return Html::a(Yii::t('app','Unpaid'), $url,['class' => 'btn btn-danger']);
+                            } elseif  ($provider['paid'] = $provider['unit_price']){
+                               return Html::a(Yii::t('app','Paid'), $url,['class' => 'btn btn-success']);
+                            } elseif  ($provider['paid'] > $provider['unit_price']){
+                               return Html::a(Yii::t('app','Paid'), $url,['class' => 'btn btn-success']);
                             }
                             },
                     ],
@@ -133,7 +163,21 @@ $provider = new ArrayDataProvider([
                         }
                     }
          ],
-        'unit_price',       
+        'unit_price',
+        [
+            'class'=>'kartik\grid\DataColumn',
+            'attribute'=>'invoice_id',
+            'header'=>'Invoice Number',
+            'value'=>function($provider){
+               if (empty($provider['invoice_id'])){
+                   return "";
+               }
+               else
+               {
+                   return $provider['invoice_id'];
+               };
+            }
+        ],      
     ],
 ]); 
 ?>
