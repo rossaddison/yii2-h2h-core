8738c864576bc7aa2ac8a19cea72c9c9e7f1e836
Ross Addison <rossaddison@users.noreply.github.com>
2021-05-03 17:31:16 +0100

    Update params.php

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,12 +1,13 @@
 <?php
 return [
-    'adminEmail' => 'email@domain.co.uk',
     'supportEmail' => 'supportemail@domain.co.uk',
+    'adminEmailFromName'=>'My Company',
+    'adminEmailFromEmail'=>'mycompany@company.com',    
     'user.passwordResetTokenExpire' => 3600,
     'icon-framework' => 'fa',
     'bsDependencyEnabled' => true, 
     'bsVersion' => '4.x',
     //when the site is undergoing routine maintenance set to 
     'portalMode'=>'non-maintenance',    
-    'DialingCodeRestriction'=>'+44'
+    'DialingCodeRestriction'=>'+44',    
 ];
